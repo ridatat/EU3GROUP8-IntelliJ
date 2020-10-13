0ea1e87293bd85e4d0501eeb7aee22977eac634c
Hayriyey <hayriyeyilmaz1981@gmail.com>
2020-10-13 23:14:04 +0300

    Added smt

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
+
         System.out.println("Hello");
         System.out.println("Hello from Msytrk ");
         System.out.println("Hello from Msytrk "); // second commit//how to create a conflict?
@@ -7,7 +8,7 @@ public class Hello {
 
         System.out.println("Each group member created one file!");
 
-        System.out.println("\"guys I am going out to another lesson see you later \" = " + "guys I am going out to another lesson see you later ");
+        System.out.println("\"guys I am going out to another lesson see you later \" = " + "guys I am going out to another lesson see you later "); System.out.println("Hello from Hayriye ");
     }
 }
 
