ecaa0b7d4198facc9daee68c791a908230252ed9
Hayriyey <hayriyeyilmaz1981@gmail.com>
2020-10-13 23:19:05 +0300

    Added smt2

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -9,6 +9,12 @@ public class Hello {
         System.out.println("Each group member created one file!");
 
         System.out.println("\"guys I am going out to another lesson see you later \" = " + "guys I am going out to another lesson see you later "); System.out.println("Hello from Hayriye ");
+        System.out.println("again me!");
+
+
     }
+
+
+
 }
 
